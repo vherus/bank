797c1bd450ffcbc70c4d554e8691fc1c7d7d77ef
Nathan King <nathanking3590@gmail.com>
2024-07-12 20:02:04 +0100

    update migration down

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,4 +1,9 @@
-DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS accounts;
+--DROP TABLE IF EXISTS "transaction";
+DROP TABLE IF EXISTS account_scheme;
+DROP TABLE IF EXISTS account;
 DROP TYPE IF EXISTS AccountType;
-DROP TABLE IF EXISTS customers;
+DROP TYPE IF EXISTS AccountSubType;
+DROP TYPE IF EXISTS AccountStatus;
+DROP TYPE IF EXISTS SwitchStatus;
+DROP TYPE IF EXISTS SchemeName;
+DROP TABLE IF EXISTS customer;
